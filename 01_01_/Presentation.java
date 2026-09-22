47a9626ec5fb1f89f325dd612a2e49931af4af12
Josetp <josetpayestas@gmail.com>
2026-09-22 09:45:27 +0200

    Git

---
+++ 01_01_/Presentation.java
@@ -0,0 +1,16 @@
+public class Presentation {
+    public static void main(String[] args) {
+
+        String name = "John";
+        String surname = "Doe.";
+        int age = 33;
+        int address = 123;
+        float height =  1.82f;
+
+        System.out.println("Hello! My name is " + name + " " + surname);
+        System.out.println("I am " + age + " y/o." );
+        System.out.println("My address is " + address + " First Avenue, London, WC1 6HB.");
+        System.out.println("My height is " + height + " meters.");
+ 
+    }
+}
