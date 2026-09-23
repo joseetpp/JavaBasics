4aa71d1df05c3860d118c2707d555497ec1c8ddc
Josetp <josetpayestas@gmail.com>
2026-09-23 11:52:02 +0200

    Exercucu 01_04

---
+++ 01_04_Hours2Seconds/Hours2Seconds.java
@@ -0,0 +1,15 @@
+public class Hours2Seconds{
+	public static void main(String[] args) {
+		int hours = 12;
+		int seconds = 43200;
+		System.out.println(hours + " hours are " + seconds + " seconds");
+
+        hours = 15;
+		seconds = 54000;
+		System.out.println( hours + " hours are " + seconds + " seconds");
+
+		hours = 23;
+		seconds = 82800;
+		System.out.println(hours + " hours are " + seconds + " seconds");
+    }
+}
